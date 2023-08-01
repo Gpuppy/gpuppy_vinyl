55fe5e130bc8cc7a12b22642e3153e5604ebb728
Gael Adam <gaelswim@hotmail.com>
2023-08-01 17:07:29 +0200

    app.js removed

--- assets/app.js
+++ assets/app.js
@@ -5,11 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
+/* any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 
-require('bootstrap')
+//require('bootstrap')
 
 // start the Stimulus application
 import './bootstrap';
@@ -17,4 +17,4 @@ import './bootstrap';
 // assets/app.js
 import { registerReactControllerComponents } from '@symfony/ux-react';
 registerReactControllerComponents(require.context('./react/controllers', true, /\\.(j|t)sx?$/));
-
+*/
