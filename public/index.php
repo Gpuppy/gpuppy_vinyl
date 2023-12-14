4a33370c83f6dafcbf2d69ca9d6b1607f0584ad3
Gael Adam <gaelswim@hotmail.com>
2023-12-14 11:57:34 +0100

    update script composer

--- public/index.php
+++ public/index.php
@@ -7,6 +7,16 @@ return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
 
+//$url = getenv('JAWS-DB_DATABASE_URL');
+/*$dbparts = parse_url($url);
+
+$hostname = $dbparts['l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+$username = $dbparts['tbuhalr2fne75dzk'];
+$password = $dbparts['v15fhziyxusv3k9s'];
+$database = ltrim($dbparts['config'],'/');*/
+
+
+
 
 //Get Heroku ClearDB connection information
 /*$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
